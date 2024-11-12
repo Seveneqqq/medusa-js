79408f36e1aefef0236c56feb4c3249e17a26601
Pawel Boron <pawcio2001.boron@interia.pl>
2024-11-12 10:26:06 +0100

    Do sprawdzenia

--- apps/backend/src/api/store/customer/add-to-approval/route.ts
+++ apps/backend/src/api/store/customer/add-to-approval/route.ts
@@ -5,7 +5,7 @@ import type {
     MedusaResponse,
 } from "@medusajs/framework"
 
-import CustomerApprovedModuleService from "src/modules/customer-approved/service"
+import {createApprovalWorkflow} from "../../../../workflows/customer"
 
 dotenv.config()
 
@@ -24,13 +24,16 @@ export const POST = async (
         try {
 
             const {email} = req.body;
-
-            res.status(200).send({ email:email});
+            const { result } = await createApprovalWorkflow(req.scope).run({
+                input: req.body,
+             });
+            
+            res.status(200).send({ email:email, result: result });
             
         } catch (error) {
             res.status(500).json({
                 success: false,
-                message: error.message,
+                message: error,
             })
         }
     })
