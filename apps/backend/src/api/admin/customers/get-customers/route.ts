ae311d5410ceaa16e14e6269984d2211b9c346bc
Pawel Boron <pawcio2001.boron@interia.pl>
2024-11-12 14:43:39 +0100

    Wyswietlanie i aktulizowanie uzytkownikow

--- apps/backend/src/api/admin/customers/get-customers/route.ts
+++ apps/backend/src/api/admin/customers/get-customers/route.ts
@@ -1,45 +1,52 @@
-import { Pool } from "pg";
-import dotenv from 'dotenv';
+import type {
+  AuthenticatedMedusaRequest,
+  MedusaResponse,
+} from "@medusajs/framework";
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
+import { RemoteQueryFunction } from "@medusajs/framework/types";
+import CustomerApprovedModuleService from "src/modules/customer-approved/service";
 
-dotenv.config();
+export const GET = async (
+  req: AuthenticatedMedusaRequest,
+  res: MedusaResponse
+) => {
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+  try{
 
-export const GET = async (req: any, res: any) => {
-    const page = parseInt(req.query.page) || 0;
-    const limit = parseInt(req.query.limit) || 25;
-    const offset = page * limit;
+    const { page, limit } = req.query;
 
-    try {
-        const countQuery = `
-            SELECT COUNT(*) 
-            FROM customer 
-            WHERE approved = false
-        `;
-        const countResult = await pool.query(countQuery);
-        const totalCount = parseInt(countResult.rows[0].count);
+    // Konwersja string na number
+    const pageNum = parseInt(page as string) || 0;
+    const limitNum = parseInt(limit as string) || 20;
+    const skip = limitNum * pageNum;
 
-        const sql = `
-            SELECT id, company_name, email, created_at, approved 
-            FROM customer 
-            WHERE approved = false 
-            ORDER BY created_at DESC
-            LIMIT $1 OFFSET $2
-        `;
-        const result = await pool.query(sql, [limit, offset]);
+    const query = req.scope.resolve<RemoteQueryFunction>(
+      ContainerRegistrationKeys.QUERY
+   );
 
-        res.status(200).json({
-            data: result.rows,
-            meta: {
-                page,
-                limit,
-                total: totalCount,
-                pageCount: Math.ceil(totalCount / limit)
-            }
-        });
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
+   const { data: customers_approved, metadata } = await query.graph({
+    entity: "customer_approved",
+    fields: ['*'],
+    pagination: {
+      skip: skip,
+      take: limitNum,
+    },
+  });
+
+  console.log(customers_approved)
+
+  res.json({
+    customers_approved,
+    // count: metadata!.count,
+    // offset: metadata!.skip,
+    // limit: metadata!.take,
+  });
+
+  }catch(err){
+    console.log(err);
+    res.status(500).json({"error": err});
+  }
+  
 };
+
+
