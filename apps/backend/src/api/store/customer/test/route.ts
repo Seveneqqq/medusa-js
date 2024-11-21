68c2ba683ff2e33c59789b515cbfd03f761652b5
Pawel Boron <pawcio2001.boron@interia.pl>
2024-11-21 09:16:18 +0100

    Linki pomiedzy modulami customer <-> customer_approved, endpoint do przetestowania zwracacanych wartosci

---
+++ apps/backend/src/api/store/customer/test/route.ts
@@ -0,0 +1,68 @@
+
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import type {
+    AuthenticatedMedusaRequest,
+    MedusaResponse,
+} from "@medusajs/framework";
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
+import { CUSTOMER_APPROVED_MODULE } from 'src/modules/customer-approved';
+import { RemoteQueryFunction } from "@medusajs/framework/types";
+import CustomerApprovedModuleService from "src/modules/customer-approved/service";
+
+dotenv.config();
+
+const corsOptions = {
+    origin: process.env.STORE_CORS,
+    credentials: true,
+};
+
+export const GET = async (
+    req: AuthenticatedMedusaRequest,
+    res: MedusaResponse
+) => {
+    cors(corsOptions)(req, res, async () => {
+        
+        try{
+        const query = req.scope.resolve<RemoteQueryFunction>(
+            ContainerRegistrationKeys.QUERY
+         );
+      
+         const email  = req.query.email;
+
+         const { data: [customer_approved] } = await query.graph(
+            {
+              entity: "customer",
+              fields: ["id", "email", "first_name", "last_name", "customer_approved.approved"],
+            },
+            { throwIfKeyNotFound: true }
+          );
+      
+        //  const customerApprovedModuleService =
+        //     req.scope.resolve<CustomerApprovedModuleService>(
+        //        "customerApprovedModuleService"
+        //     );
+      
+        //  const customer_approved =
+        //     await customerApprovedModuleService.listCustomerApproveds({
+        //        email: email,
+        //     },
+        //     {
+        //       select: ["email", "approved"],
+        //     }
+        //   );
+      
+         res.status(200).json({
+            customer_approved
+         });
+        }catch(error){
+            res.status(500).json({ message: error.message });
+        }
+
+    });
+};
+
+export const OPTIONS = cors(corsOptions);
+
+export const CORS = false;
